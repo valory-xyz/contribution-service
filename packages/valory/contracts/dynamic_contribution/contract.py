325816cc1dfd63a4970a26ffc7cc7e7415351342
David Vilela <dvilelaf@gmail.com>
2022-11-30 09:21:25 +0100

    fix: update event id key

--- packages/valory/contracts/dynamic_contribution/contract.py
+++ packages/valory/contracts/dynamic_contribution/contract.py
@@ -113,7 +113,7 @@ class DynamicContributionContract(Contract):
         ).get_all_entries()  # limited to 10k entries for now: https://github.com/valory-xyz/contribution-service/issues/13
 
         member_to_token_id = {
-            entry["args"]["to"]: int(entry["args"]["tokenId"]) for entry in entries
+            entry["args"]["to"]: int(entry["args"]["id"]) for entry in entries
         }
 
         return dict(member_to_token_id=member_to_token_id)
